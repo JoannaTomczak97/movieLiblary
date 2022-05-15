52048711a9e288e8e3f1cfdf5cbcc91be24e962c
JoannaTomczak97 <jtomczak97@gmail.com>
2022-05-15 12:10:26 +0200

    change2 commit

---
+++ src/main/java/MovieLiblaryApp.java
@@ -0,0 +1,8 @@
+import models.Actor;
+
+public class MovieLiblaryApp {
+    public static void main(String[] args) {
+        Actor brad = new Actor("Brad", "Pit");
+        System.out.println(brad);
+    }
+}
--- src/main/java/org/example/MovieLiblaryApp.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class MovieLiblaryApp {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
